ead6ff010461fcbdd5784c31eb2fe997cc61605e
Dunridge <Polinka7max@gmail.com>
2021-01-17 11:51:50 +0200

    took apart an rxjs guide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {combineLatest, fromEvent, Observable} from 'rxjs';
+import {map, scan, throttleTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-tutorial';
+
+  ngOnInit(): void {
+    const button = document.querySelector('button');
+    // .addEventListener('click', () => console.log('clicked'));
+
+    // fromEvent(button, 'click')
+    //   .subscribe(() => console.log('clicked!!!'));
+
+    // let count = 0;
+    // button.addEventListener('click', () => console.log(`clicked ${++count} times`));
+
+    // fromEvent(button, 'click').pipe(
+    //  scan((countNum) => countNum + 1, 0)
+    // ).subscribe((countNum) => console.log(`clicked ${countNum} times`));
+
+    // fromEvent(button, 'click').pipe(
+    //   throttleTime(1000),
+    //   scan((countNum) => countNum + 1, 0)
+    // ).subscribe((countNum) => console.log(`clicked ${countNum} times`));
+
+    fromEvent(button, 'click').pipe(
+      throttleTime(1000),
+      map((event: MouseEvent) => event.clientX),
+      scan((countNum, clientX) => countNum + clientX, 0)
+    ).subscribe((countNum) => {
+      // console.log(`clicked ${countNum} times`);
+      console.log(countNum);
+    });
+  }
 }
